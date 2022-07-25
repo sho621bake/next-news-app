1bd603c667b84b838d3bd06a6a18bcff5cad95d9
sho621bake <sho6217@gmail.com>
2022-07-25 17:06:28 +0900

    Fixed Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home(props) {
     return (
         <MainLayout>
             <Head>
-                <title>Simple News</title>
+                <title>Next News</title>
             </Head>
         </MainLayout>
     )
